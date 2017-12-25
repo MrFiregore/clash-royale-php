b7c28a36f96882e3bdacc01e7fc50e783c802c70
Firegore <firegore2@gmail.com>
2017-12-25 19:13:58 +0100

    Updated for new rows
    added new donations rows and modify the role_name to role

--- examples/clanProfiles.php
+++ examples/clanProfiles.php
@@ -3,30 +3,42 @@ ini_set('max_execution_time', 3000);
 use CR\Api;
 require '../vendor/autoload.php';
 $api = new Api();
+try {
+  $clan = $api->getClan(["YQ02QJ"]);
+  foreach ($clan->getMembers() as $profile) {
+    $name = $profile->getName();
+    $tag = $profile->getTag();
+    $arena = $profile->getArena();//Arena Object
+    $role = $profile->getRole();
+    $exp_level = $profile->getexpLevel();
+    $trophies = $profile->getTrophies();
+    $rank = $profile->has("rank") ?  $profile->getRank() : false ;
+    $previous_rank = $profile->has("previousRank") ? $profile->getPreviousRank() : false;
+    $clan_chest_crowns= $profile->getClanChestCrowns();
+    $score = $profile->getScore();
+    $donations = $profile->getDonations();
+    $donations_received = $profile->getDonationsReceived();
+    $donations_delta = $profile->getDonationsDelta();
+    $donations_percent = $profile->getDonationsPercent();
 
-foreach ($api->getClan(["YQ02QJ"])->getMembers() as $profile) {
-  $name = $profile->getName();
-  $arena = $profile->getArena();//Arena Object
-  $role = $profile->getRole();
-  $role_name = $profile->getRoleName();
-  $exp_level = $profile->getexpLevel();
-  $trophies = $profile->getTrophies();
-  $donations = $profile->getDonations();
-  $current_rank = $profile->has("currentRank") ?  $profile->getCurrentRank() : false ;
-  $previous_rank = $profile->has("previousRank") ? $profile->getPreviousRank() : false;
-  $clan_chest_crowns= $profile->getClanChestCrowns();
-  $score = $profile->getScore();
-  d(
-    $name,
-    $arena,
-    $role,
-    $role_name,
-    $exp_level,
-    $trophies,
-    $donations,
-    $current_rank,
-    $previous_rank,
-    $clan_chest_crowns,
-    $score
-  );
+    d(
+      $profile,
+      $tag,
+      $name,
+      $arena,
+      $role,
+      $exp_level,
+      $trophies,
+      $rank,
+      $previous_rank,
+      $clan_chest_crowns,
+      $score,
+      $donations,
+      $donations_received,
+      $donations_delta,
+      $donations_percent
+    );
+  }
+} catch (\Exception $e) {
+  d($e);
 }
