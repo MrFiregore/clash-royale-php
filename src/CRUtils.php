72fde8c18a40c7e5bd37da3ae7f2158fbb94f40d
Firegore <firegore2@gmail.com>
2018-06-01 01:53:44 +0200

    Update CRUtils.php

--- src/CRUtils.php
+++ src/CRUtils.php
@@ -13,98 +13,115 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 namespace CR;
-use ReflectionMethod;
-use ReflectionFunction;
+use CR\CRCache;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use CR\HttpClients\GuzzleHttpClient;
+
 
 /**
  *
  */
-class CRUtils
+class CRVersion
 {
-    protected static $composer;
-    protected static $root;
-
-    /**
-     * @return \Composer\Autoload\ClassLoader|false
-     */
-    public static function getComposer(){
-        if (self::$composer !== false){
-            $composer_path = self::getRoot()."vendor".DIRECTORY_SEPARATOR."autoload.php";
-            self::$composer = (file_exists($composer_path)) ? require $composer_path : false;
-        }
-        return self::$composer;
-    }
-
-    /**
-     * @return string
-     */
-    public static function getRoot(){
-        if (!self::$root){
-            self::$root = substr(__DIR__,0,strpos(__DIR__,"vendor") ?: strpos(__DIR__,"src"));
-        }
-        return self::$root;
-    }
-  public static function delTree($dir) {
-    if (is_dir($dir)) {
-       $objects = scandir($dir);
-       foreach ($objects as $object) {
-         if ($object != "." && $object != "..") {
-           if (is_dir($dir."/".$object))
-             self::delTree($dir."/".$object);
-           else
-             unlink($dir."/".$object);
-         }
-       }
-       rmdir($dir);
-     }
-   }
+  const API_VERSION = "1.3.1";
   /**
-   * Check if the given string is a HTML page
-   * @method isHTMLPage
-   * @param  string     $string The string to check
-   * @return bool               Returns true if is a HTML page, otherwise returns false
+   * @var GuzzleHttpClient HTTP Client
    */
-  public static function isHTMLPage($string)
-  {
-    return preg_match('/<html.*>/', $string) != 0;
-  }
-  public static function getFunctionDefaultValues()
-  {
-    $values = [];
-    $debug_backtrace = debug_backtrace();
+  protected static $httpClientHandler;
 
-    if (!isset($debug_backtrace[1])) {
-      return $values;
+
+
+  /**
+   * Returns the HTTP client handler.
+   *
+   * @return GuzzleHttpClient
+   */
+  protected static function getHttpClientHandler()
+  {
+    if (is_null(self::$httpClientHandler)) {
+      $client = new Client();
+
+      self::$httpClientHandler = new GuzzleHttpClient($client);
+    }
+      return self::$httpClientHandler;
+  }
+  /**
+   * [checkGithub description]
+   * @param  string $version [description]
+   * @return [type]          [description]
+   */
+  public static function checkGithub(string $version)
+  {
+
+    $rawResponse = self::getHttpClientHandler()->send(
+      "https://api.github.com/repos/firegore2/clash-royale-php/releases/tags/".$version,
+      "GET",
+      [RequestOptions::HEADERS=>["Accept: application/vnd.github.v3+json"]],
+      30,
+      false,
+      60
+    );
+    return $rawResponse->getStatusCode() == 200 ? $rawResponse->getBody()->getContents() : false;
+  }
+  /**
+   * [checkPackagist description]
+   * @return [type] [description]
+   */
+  public static function checkPackagist()
+  {
+
+    $rawResponse = self::getHttpClientHandler()->send(
+      "https://packagist.org/p/firegore2/clash-royale-php.json",
+      "GET",
+      [],
+      30,
+      false,
+      60
+    );
+    return $rawResponse->getStatusCode() == 200 ? $rawResponse->getBody()->getContents() : false;
+  }
+
+  /**
+   * [checkVersion description]
+   * @return [type] [description]
+   */
+  public static function checkVersion()
+  {
+    if (!CRCache::exists("APIVERSION".self::API_VERSION) || version_compare(self::API_VERSION,CRCache::get("APIVERSION".self::API_VERSION),">")) {
+      CRUtils::delTree(CRCache::getPath());
+      CRCache::write("APIVERSION".self::API_VERSION,self::API_VERSION);
     }
 
-    $caller = $debug_backtrace[1];
-    $type   = (isset($caller['class']) ? "Method" : "Function");
-    $func_name = ($type === "Method") ? $caller['class']."::".$caller['function'] : $caller['function'];
+    if (!CRCache::exists("checkVersion", ["maxage"=>60])) {
+      CRCache::write("checkVersion","1");
+      if ($packagist = self::checkPackagist()) {
+        $packagist = collect(json_decode($packagist,true)['packages']['firegore2/clash-royale-php'])
+        ->reject(function ($item,$key)
+        {
+          return strpos($key, "dev-") !== false ;
+        });
 
+        $max_version = $packagist->max("version");
+        d($max_version);
+        if (version_compare($max_version,self::API_VERSION,">")) {
+          $new_version = $packagist->get($max_version);
+          unset($packagist);
+          $alert = "New version ** $max_version ** available of ** ".$new_version['name']." **  - ".$new_version['description']."\n";
 
-    $reflection_type = "Reflection".$type;
-    $reflection = new $reflection_type($func_name);
-
-
-    foreach ($reflection->getParameters() as $param) {
-
-      $arg_sent = isset($caller["args"][$param->getPosition()]) && !is_null($caller["args"][$param->getPosition()])  ?
-                    $caller["args"][$param->getPosition()] :
-                    (
-                      ($param->isOptional() && $param->isDefaultValueAvailable()) ?
-                          $param->getDefaultValue() : null
-                    );
-
-      $values[$param->getName()] = $arg_sent;
-
+          if ($github = self::checkGithub($max_version)) {
+            $github = json_decode($github,true);
+            $alert .= "###".$github['name']."\n ---\n".$github['body']." \n ---\n";
+          }
+          $alert .= "Run `composer update firegore2/clash-royale-php` to update the package.\n";
+          echo CRUtils::markdownToHTML($alert);
+        }
+      }
     }
 
-    return $values;
 
   }
-  public static function isAssoc(array $arr)
-  {
-    if([] === $arr) return false;
-    return array_keys($arr) !== range(0, count($arr) - 1 );
-  }
+
+
+
 }
